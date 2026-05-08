b781be56888fe4e39e50817eea03b0c295d4bde9
vDevopsvinod <your-email@example.com>
2026-05-08 16:10:39 +0530

    initial eks cicd project

---
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "DevOps App Running on EKS";
+    }
+}
